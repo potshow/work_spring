d3542a89ad02ddc4c3062a5ba743da114e8f2500
KOITT <KOITT@KOITT-PC14>
2018-03-02 17:49:28 +0900

    일정 시간마다 특정 메세지 출력하는 프로그램 만듬

--- SpringEtc04Task/src/com/koitt/schedule/MySchedule.java
+++ SpringEtc04Task/src/com/koitt/schedule/MySchedule.java
@@ -1,5 +1,14 @@
 package com.koitt.schedule;
 
-public class MySchedule {
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
+public class MySchedule {
+	
+	@Scheduled(cron="0/5 * * * * *")	// 1q분마다 실행
+	public void printTest() {
+		System.out.println("5초마다 출력되는 메세지");
+	}
+	
 }
